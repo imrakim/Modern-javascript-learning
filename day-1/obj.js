76abac9ea2e64d7744d9b7c06295e21131c78acf
rakim <rakimali770@gmail.com>
2024-06-11 13:20:17 +0600

    adding and delete both use

--- day-1/obj.js
+++ day-1/obj.js
@@ -6,6 +6,15 @@ details.lastName = "Ali";
 details.age = 21;
 details.schoolName = "Uttara High School And College";
 
+details.nationality = "Bengali"
+
+delete details.age
 const connect = document.getElementById("test");
 connect.innerHTML =
-  details.fristName + " "+  details.lastName + " is " + details.age + " years old. ";
+  details.fristName +
+  " " +
+  details.lastName +
+  " is " +
+  details.age +
+  " years old. " +
+  details.nationality;
