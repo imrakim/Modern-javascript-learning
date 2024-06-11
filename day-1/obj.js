b496ac8818fedd5084f6c250994c8f3e5bd07250
rakim <rakimali770@gmail.com>
2024-06-11 13:41:04 +0600

    learning object advance method

--- day-1/obj.js
+++ day-1/obj.js
@@ -6,9 +6,9 @@ details.lastName = "Ali";
 details.age = 21;
 details.schoolName = "Uttara High School And College";
 
-details.nationality = "Bengali"
+details.nationality = "Bengali";
 
-delete details.age
+delete details.age;
 const connect = document.getElementById("test");
 connect.innerHTML =
   details.fristName +
@@ -18,3 +18,15 @@ connect.innerHTML =
   details.age +
   " years old. " +
   details.nationality;
+
+const useThis = {
+    fristName:"Rakim",
+    lastName :"Ali",
+    age : 21,
+    schoolName:"Uttara High School And College",
+    fullName : function(){
+        return this.fristName + " " + this.lastName
+    }
+};
+let again = document.getElementById('mdtest');
+again.innerHTML=useThis.fullName();
